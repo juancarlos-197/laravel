9bf077388277d3354afaf956f105b80f99474d53
juancarlos-197 <juancarlos-197@unimayor.edu.co>
2025-05-20 11:46:15 -0500

    Ajuste  un curso laravel

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    
-   // return view('welcome');
-    return "Bienvenido" ;
 
+    // return view('welcome');
+    return "Bienvenido";
 });
 
 Route::get('cursos', function () {
-    return "Bienvenido al curso" ;
+    return "Bienvenido al curso";
 });
 Route::get('cursos/create', function () {
     return "En esta página podria crear un curso";
 });
-Route::get('cursos/{curso}', function ($curso) {
-    return "Bienvenido al curso: $curso" ;
+/** Route::get('cursos/{curso}', function ($curso) {
+ *   return "Bienvenido al curso: $curso" ;
+ *});
+ */
+
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria) {
+        return "Bienvenido al curso: $curso, de la categoria $categoria ";
+    } else {
+        return "Bienvenido al curso: $curso";
+    }
 });
